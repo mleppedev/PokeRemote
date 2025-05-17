e342b6c2fbd2ef46e17e5ea5fad71b8e41fa38df
TrineoApp SpA <contacto@trineoapp.cl>
2025-05-17 14:01:43 -0400

    rereload

--- client/node_modules/react-native-webrtc/tools/downloadBitcode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Bitcode is deprecated since Xcode 14."
-echo "The current WebRTC build already contains bitcode."
-echo "Soon this file will disappear and so will bitcode builds."
